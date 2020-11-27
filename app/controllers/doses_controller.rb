c96de2f3ccd8d0cab8d56cde01049a4fd4012073
Athiley Luciano <athileyluciano@gmail.com>
2020-11-27 08:50:52 +0100

    Mr cocktails progress...

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,11 +6,11 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @doses = Dose.new(dose_params)
+    @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     @dose.save
 
-    redirect_to cocktail_path(@dose.cocktail)
+    redirect_to cocktail_path(@cocktail)
   end
 
   def dose_params
